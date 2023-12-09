e52ce20edb6d7950ca6675c28a30ba383da15017
TeaTeaKay <teateakay@gmail.com>
2023-12-09 13:53:22 +0000

    Initial Commit

---
+++ src/lib/Slides.svelte
@@ -0,0 +1,19 @@
+<script>
+    import 'reveal.js/dist/reveal.css';
+    import 'reveal.js/dist/theme/night.css';
+
+    import Reveal from 'reveal.js';
+    import {onMount, tick} from 'svelte';
+
+    onMount(async () => {
+        await tick();
+        const deck = new Reveal({hash: true, slideNumber: true});
+        deck.initialize();
+    });
+</script>
+
+<div class="reveal">
+    <div class="slides">
+        <slot/>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Slides from '$lib/Slides.svelte';
+    import Title from '$lib/assets/QuizTitle.png';
+</script>
+
+<Slides>
+    <section data-auto-animate data-auto-animate-restart>
+        <div style="position:relative">
+            <img data-id="logo" src={Title} style="translate:0 -100vh" />
+        </div>
+        <p>Are you ready?</p>
+    </section>
+    <section data-auto-animate>
+        <div style="position:relative">
+            <img data-id="logo" src={Title} style="translate:0 0" />
+        </div>
+        <p>Let's GO!</p>
+    </section>
+    <section>
+        <h1>Slide 2</h1>
+    </section>
+</Slides>
