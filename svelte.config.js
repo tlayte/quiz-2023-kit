d244f41bbd35f776efe882a32b0bf815086ac7c0
TeaTeaKay <teateakay@gmail.com>
2023-12-09 21:33:02 +0000

    Disable pointless a11y warnings for a project only used by me

--- svelte.config.js
+++ svelte.config.js
@@ -2,20 +2,24 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(
-			{
-				pages: 'build',
-				assets: 'build',
-				fallback: undefined,
-				precompress: false,
-				strict: true
-			}
-		)
-	}
+    onwarn: (warning, handler) => {
+        if (warning.code.startsWith('a11y-')) return;
+        handler(warning);
+    },
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(
+            {
+                pages: 'build',
+                assets: 'build',
+                fallback: undefined,
+                precompress: false,
+                strict: true
+            }
+        )
+    }
 };
 
 export default config;
