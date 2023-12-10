4139cb19583bc424829117003dc859470ddd346d
TeaTeaKay <teateakay@gmail.com>
2023-12-10 02:30:59 +0000

    Switch yarn to pnp

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+
+		server: {
+			fs: {
+				strict: false
+			}
+		},
 	plugins: [
 		sveltekit()
 	]
