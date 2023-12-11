97ece7402a872d18ca634f8e8495ac5083c551d3
TeaTeaKay <teateakay@gmail.com>
2023-12-11 17:10:28 +0000

    Small tweaks

--- src/lib/rounds/Art/FamousPeople.svelte
+++ src/lib/rounds/Art/FamousPeople.svelte
@@ -2,42 +2,61 @@
     export let questionCount;
 </script>
 
-<section data-question-number={++questionCount}>
+<section>
     <h2>Famous Artists</h2>
     <aside class="notes">
         Sort these famous artists in ascending order by year of birth
     </aside>
 </section>
 
-<section data-question-number={questionCount}>
+<section data-question-number={++questionCount} data-auto-animate>
+    <h4>In order of year of birth</h4>
     <ol class="people">
         <li>Michelangelo</li>
         <li>Pablo Picasso</li>
         <li>Leonardo da Vinci</li>
         <li>James McNeill Whistler</li>
-        <li>David Hockney</li>
-        <li>Andy Warhol</li>
-        <li>L. S. Lowry</li>
-        <li>Peter Paul Rubens</li>
-        <li>Claude Monet</li>
-        <li>Vincent van Gogh</li>
-        <li>Pierre-Auguste Renoir</li>
-        <li>Henri de Toulouse-Lautrec</li>
     </ol>
     <aside class="notes">
         <table>
             <tr><td>Leonardo da Vinci</td><td>1452</td></tr>
             <tr><td>Michelangelo</td><td>1475</td></tr>
-            <tr><td>Peter Paul Rubens</td><td>1577</td></tr>
             <tr><td>James McNeill Whistler</td><td>1834</td></tr>
-            <tr><td>Claude Monet</td><td>1840</td></tr>
-            <tr><td>Pierre-Auguste Renoir</td><td>1841</td></tr>
-            <tr><td>Vincent van Gogh</td><td>1853</td></tr>
-            <tr><td>Henri de Toulouse-Lautrec</td><td>1864</td></tr>
             <tr><td>Pablo Picasso</td><td>1881</td></tr>
-            <tr><td>L. S. Lowry</td><td>1887</td></tr>
+        </table>
+    </aside>
+</section>
+<section data-question-number={++questionCount} data-auto-animate>
+    <h4>In order of year of birth</h4>
+    <ol class="people">
+        <li>Claude Monet</li>
+        <li>David Hockney</li>
+        <li>Henri de Toulouse-Lautrec</li>
+        <li>Andy Warhol</li>
+    </ol>
+    <aside class="notes">
+        <table>
+            <tr><td>Claude Monet</td><td>1840</td></tr>
+            <tr><td>Henri de Toulouse-Lautrec</td><td>1864</td></tr>
             <tr><td>Andy Warhol</td><td>1928</td></tr>
             <tr><td>David Hockney</td><td>1937</td></tr>
+        </table>
+    </aside>
+</section>
+<section data-question-number={++questionCount} data-auto-animate>
+    <h4>In order of year of birth</h4>
+    <ol class="people">
+        <li>L. S. Lowry</li>
+        <li>Peter Paul Rubens</li>
+        <li>Vincent van Gogh</li>
+        <li>Pierre-Auguste Renoir</li>
+    </ol>
+    <aside class="notes">
+        <table>
+            <tr><td>Peter Paul Rubens</td><td>1577</td></tr>
+            <tr><td>Pierre-Auguste Renoir</td><td>1841</td></tr>
+            <tr><td>Vincent van Gogh</td><td>1853</td></tr>
+            <tr><td>L. S. Lowry</td><td>1887</td></tr>
         </table>
     </aside>
 </section>
