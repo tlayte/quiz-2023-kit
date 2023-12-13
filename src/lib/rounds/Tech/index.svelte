2ce3fbe59e4fdf8442900536c951a73276e1e110
TeaTeaKay <teateakay@gmail.com>
2023-12-13 10:57:06 +0000

    rename tech round and add more questions

--- src/lib/rounds/Tech/index.svelte
+++ src/lib/rounds/Tech/index.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-    export const Name="Tech Support";
+    export const Name = "Nerdy Stuff";
 </script>
 <script>
     import Round from '$lib/util/Round.svelte';
@@ -31,27 +31,10 @@ int main() {
     </pre>
     </section>
     <section>
-        <h2>Fun with maths</h2>
-    </section>
-    <section data-question-number={++questionCount}>
-        <h4>Train danger!</h4>
-        <ul>
-            <li class="fragment">A train leaves Penzance station travelling at 90mph</li>
-            <li class="fragment">Another train leaves Paddington travelling at 60mph</li>
-            <li class="fragment">Both trains leave at exactly 10am</li>
-            <li class="fragment">The stations are 250 miles apart</li>
-            <li class="fragment">Assuming there are no other trains, what is 4 + 4?</li>
-        </ul>
-        <aside class="notes">
-            4+4 = 8<br/>
-            The trains meet at 11:40am, 150 miles from Penzance and 100 miles from Paddington.
-        </aside>
-
-    </section>
-    <section>
         <h2>HTTP Errors</h2>
         <aside class="notes">
-            I'm sure you've all seen these errors before when a website you were looking for broke in some way. But what do they mean?
+            I'm sure you've all seen these errors before when a website you were looking for broke in some way. But what
+            do they mean?
         </aside>
     </section>
     <section data-question-number={++questionCount}>
@@ -68,11 +51,61 @@ int main() {
             500: internal server error.
         </aside>
     </section>
+    <section>
+        <h2>Fun with maths</h2>
+    </section>
+    <section data-question-number={++questionCount}>
+        <h4>Train danger!</h4>
+        <ul>
+            <li class="fragment">A train leaves Penzance station travelling at 90mph</li>
+            <li class="fragment">Another train leaves Paddington travelling at 60mph</li>
+            <li class="fragment">Both trains leave at exactly 10am</li>
+            <li class="fragment">The stations are 250 miles apart</li>
+            <li class="fragment">Assuming there are no other trains, what is 4 + 4?</li>
+        </ul>
+        <aside class="notes">
+            4+4 = 8<br/>
+            The trains meet at 11:40am, 150 miles from Penzance and 100 miles from Paddington.
+        </aside>
+    </section>
+    <section>
+        <h2>Acronyms</h2>
+    </section>
+    <section data-question-number={++questionCount} style="position: relative">
+        <h4>WTF is all this? IDEK and IDGAF, FML!</h4>
+        <ol class="circle-list short-columns">
+            <li>USB</li>
+            <li>RAM</li>
+            <li>BIOS</li>
+            <li>HTML</li>
+            <li>TWAIN</li>
+            <li>PEBCAK</li>
+            <li>RTFM</li>
+            <li>PHP</li>
+        </ol>
+        <aside class="notes">
+            <ol style="list-style-type: upper-alpha; font-size:1rem;">
+                <li>Universal Serial Bus</li>
+                <li>Random Access Memory</li>
+                <li>Basic Input Output System</li>
+                <li>HyperText Markup Language</li>
+                <li>Technology Without An Interesting Name</li>
+                <li>Problem Exists Between Chair And Keyboard</li>
+                <li>Read the "Freaking" Manual</li>
+                <li>PHP: Hypertext Preprocessor</li>
+            </ol>
+        </aside>
+
+    </section>
 
 </Round>
 
 <style>
     ul {
         font-size: 1.5rem;
+    }
+
+    .short-columns {
+        height: 15rem;
     }
 </style>
