8a3e516cc4a6c3a50337e3f3c48a6194260724d5
TeaTeaKay <teateakay@gmail.com>
2023-12-14 14:15:48 +0000

    Art and food answers

--- src/lib/rounds/Art/Answers.svelte
+++ src/lib/rounds/Art/Answers.svelte
@@ -32,23 +32,23 @@
         </ol>
     </li>
     <li class="span-4">
-        <ol class="circle-list grid smallest numeric">
-            <li>Leonardo da Vinci - 1452</li>
+        <ol class="circle-list grid smaller numeric">
+            <li style="padding-right: 0; padding-left: 0;">Leonardo da Vinci - 1452</li>
             <li>Michelangelo - 1475</li>
             <li>James Whistler - 1834</li>
             <li>Pablo Picasso - 1881</li>
         </ol>
     </li>
     <li class="span-4">
-        <ol class="circle-list grid smallest numeric">
+        <ol class="circle-list grid smaller numeric">
             <li>Claude Monet - 1840</li>
-            <li>Toulouse-Lautrec - 1864</li>
+            <li style="padding-right: 0">Toulouse-Lautrec - 1864</li>
             <li>Andy Warhol - 1928</li>
             <li>David Hockney - 1937</li>
         </ol>
     </li>
     <li class="span-4">
-        <ol class="circle-list grid smallest numeric">
+        <ol class="circle-list grid smaller numeric">
             <li>Rubens - 1577</li>
             <li>Renoir - 1841</li>
             <li>Van Gogh - 1853</li>
--- src/lib/rounds/Food/Answers.svelte
+++ src/lib/rounds/Food/Answers.svelte
@@ -1,4 +1,91 @@
-<li>Stuff in here</li>
-<li>More stuff</li>
-<li>More stuff</li>
-<li>More stuff</li>
+<ol class="answer-grid">
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Panades - Belize</li>
+            <li>Karelian Pies - Finland</li>
+            <li>Calzone - Italy</li>
+            <li>Cornish Pasties - UK</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Fry Jacks - Belize</li>
+            <li>Spaetzle - Germany</li>
+            <li>Momos - India</li>
+            <li>Pierogi - Poland</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Waffles - Belgium</li>
+            <li>Poffertjes - Netherlands</li>
+            <li>Yorkshire Puddings - UK</li>
+            <li>Pancakes - USA</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Nanaimo Bars - Canada</li>
+            <li>Bobotie - South Africa</li>
+            <li>Smörgåstårta - Sweden</li>
+            <li>Friends Trifle - USA (Rachel)</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Democracy Sausage - Australia</li>
+            <li>Bratwurst & Sauerkraut - DE</li>
+            <li>Square Sausage - Scotland</li>
+            <li>Bangers & Mash - UK</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Frites & Mayo - Belgium</li>
+            <li>Poutine - Canada</li>
+            <li>Rosti - Switzerland</li>
+            <li>Steak Fries - USA</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Bush Tucker - Australia</li>
+            <li>Perigord Truffle - France</li>
+            <li>Samphire - UK</li>
+            <li>Laverbread - Wales</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Pescado Frito</li>
+            <li>Whitebait Fritters - New Zealand</li>
+            <li>Surströmming 🤮 - Sweden</li>
+            <li>Jellied Eels - UK</li>
+        </ol>
+    </li>
+    <li class="span-4">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>Sauerkraut - Germany</li>
+            <li>Kimchi - Korea</li>
+            <li>Orgorki/gherkins - Poland</li>
+            <li>Piccalilli - UK</li>
+        </ol>
+    </li>
+    <li class="span-12">
+        <ol class="circle-list circle-grid smallest columns-1">
+            <li>UK - Candyfloss</li>
+            <li>USA - Cotton Candy</li>
+            <li>France - Barbe à Papa / Daddy's Beard 😕</li>
+            <li>Spain/Germany - Sugar Cotton</li>
+            <li>Australia - Fairy Floss</li>
+        </ol>
+    </li>
+</ol>
+
+<style>
+    .answer-grid{
+        font-size: 1rem;
+        width: 95%;
+        row-gap: 1.5rem;
+    }
+</style>
